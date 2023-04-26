90532223978baeddf2502aaad2ceccecec484caf
Replit user <>
2023-04-26 06:20:52 +0000

    Declare env var for AWS Amplify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    // For AWS Amplify.
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY
+  },
 }
-
 module.exports = nextConfig
